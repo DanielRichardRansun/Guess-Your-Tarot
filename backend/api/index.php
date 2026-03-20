1c9b73a73868fd3a8f7a4c4791d9054eb279508b
native160421081 <richardgtwp@gmail.com>
2026-03-20 22:38:32 +0700

    suntik detektif error vercel

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,3 +1,12 @@
 <?php
-
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    header('Content-Type: text/plain');
+    echo "VERCEL RAW ERROR DETECTED:\n";
+    echo $e->getMessage() . "\n";
+    echo $e->getFile() . " on line " . $e->getLine() . "\n\n";
+    echo "Raw Stack Trace:\n";
+    echo $e->getTraceAsString();
+    exit;
+}
