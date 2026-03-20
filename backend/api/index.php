36cf1dc1847910005b73c107a34af653a7389255
native160421081 <richardgtwp@gmail.com>
2026-03-20 22:53:25 +0700

    perbaikan mutlak penipu folder cache vercel

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,4 +1,14 @@
 <?php
+if (isset($_SERVER['VERCEL']) || isset($_ENV['VERCEL'])) {
+    // Trik Sakti Vercel Serverless (Read-Only Filesystem Fix)
+    putenv('APP_SERVICES_CACHE=/tmp/cache/services.php');
+    putenv('APP_PACKAGES_CACHE=/tmp/cache/packages.php');
+    putenv('APP_CONFIG_CACHE=/tmp/cache/config.php');
+    putenv('APP_ROUTES_CACHE=/tmp/cache/routes-v7.php');
+    putenv('APP_EVENTS_CACHE=/tmp/cache/events.php');
+    @mkdir('/tmp/cache', 0777, true);
+}
+
 try {
     require __DIR__ . '/../public/index.php';
 } catch (\Throwable $e) {
